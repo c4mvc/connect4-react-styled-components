df7c6a0b7436da2a14e99e358f1567f00a36675a
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-05-25 11:08:08 +0200

    Added animation

--- src/PlayerLabel.js
+++ src/PlayerLabel.js
@@ -1,5 +1,38 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import { CircleRed, CircleYellow } from "./styles";
+
+const AnimatedCircleRed = styled(CircleRed)`
+  -webkit-animation-name: example; /* Safari 4.0 - 8.0 */
+  -webkit-animation-duration: 4s; /* Safari 4.0 - 8.0 */
+  animation-name: example;
+  animation-duration: 4s;
+  animation-iteration-count: 55;
+  @keyframes example {
+    from {
+      border-color: yellow;
+    }
+    to {
+      border-color: red;
+    }
+  }
+`;
+
+const AnimatedCircleYellow = styled(CircleYellow)`
+  -webkit-animation-name: example; /* Safari 4.0 - 8.0 */
+  -webkit-animation-duration: 4s; /* Safari 4.0 - 8.0 */
+  animation-name: example;
+  animation-duration: 4s;
+  animation-iteration-count: 55;
+  @keyframes example {
+    from {
+      border-color: red;
+    }
+    to {
+      border-color: yellow;
+    }
+  }
+`;
 
 export default class PlayerLabel extends Component {
   render() {
@@ -7,11 +40,11 @@ export default class PlayerLabel extends Component {
       <div className="row">
         <div className="col-md-3">
           <h3>Player1</h3>
-          <CircleRed />
+          <AnimatedCircleRed />
         </div>
         <div className="col-md-3">
           <h3>Player2</h3>
-          <CircleYellow />
+          <AnimatedCircleYellow />
         </div>
       </div>
     );
